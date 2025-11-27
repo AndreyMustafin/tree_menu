77fe9eb8a8969761ebb35ad1d34c6877b68a4daf
Andrey Mustafin <ksenolol5@gmail.com>
2025-11-28 00:17:50 +0300

    add model and admin

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    url = models.CharField(max_length=200, default='')
+    class Meta:
+        verbose_name = ('menu')
+        verbose_name_plural = ('menus')
+
+    def __str__(self):
+        return self.name
